80f09b26786c4c25fb03440f16e8c18ff81355e1
DarkZDrake <45799334+DarkZDrake@users.noreply.github.com>
2018-12-11 20:11:22 +0100

    Add files via upload

---
+++ testbot.py
@@ -0,0 +1,26 @@
+import json
+import os
+import discord
+
+from utils import music, utility
+from discord.ext import commands
+
+config = utility._import("test.json")
+songs = utility._import("data/music.json")
+bot = commands.Bot(command_prefix=config.prefix)
+bot.load_extension('utils.music')
+
+@bot.event
+async def on_ready():
+    await bot.wait_until_ready()
+    print("\nRunning...")
+
+@bot.command()
+async def test(ctx):
+    await ctx.send('I heard you! {0}'.format(ctx.author.mention))
+
+@bot.command()
+async def wotd(ctx):
+    await ctx.send('Your Waifu of the Day:')
+
+bot.run(config.token)
